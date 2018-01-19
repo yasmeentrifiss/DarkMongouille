44b93291399cf39fd55be09a08d639bb282ad4a9
Yasmeen Trifiss <yasmeen.trifiss@gmail.com>
2018-01-19 21:36:15 +0100

    Add simple connection to db

--- DarkMongouille/Program.cs
+++ DarkMongouille/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
 
 namespace DarkMongouille
 {
@@ -10,6 +13,22 @@ namespace DarkMongouille
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Connect...");
+
+            String uri = "mongodb://127.0.0.1:27017";
+
+            MongoClient client = new MongoClient(uri);
+            IMongoDatabase db = client.GetDatabase("sakila");
+
+            Console.WriteLine("Connected");
+            Console.WriteLine(db.ToString());
+            IMongoCollection<BsonDocument> staff = db.GetCollection<BsonDocument>("staff");
+
+            var document = staff.Find(new BsonDocument()).FirstOrDefault();
+            Console.WriteLine(document.ToString());
+            Console.ReadKey();
+
+
         }
     }
 }

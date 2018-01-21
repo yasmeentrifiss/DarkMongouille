bcb764eec8ebd613ee2838b587f2796a3bd7d8e3
Yasmeen Trifiss <yasmeen.trifiss@gmail.com>
2018-01-21 20:25:50 +0100

    Add category

---
+++ DarkMongouille/Category.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkMongouille
+{
+    public class Category
+    {
+
+        [BsonElement("category_id")]
+        public int Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("last_update")]
+        public string LastUpdate { get; set; }
+
+        [BsonElement("film_id")]
+        public int FilmId { get; set; }
+
+
+
+  
+    }
+}
